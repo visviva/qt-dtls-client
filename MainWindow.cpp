d50571af2851e0a8b9061a34abe7f7b0d955337f
silauser <simon@lauser.dev>
2019-11-14 09:56:57 +0100

    Add CMake project and basic Qt C++ files
    Adds CMakeLists.txt and MainWindow class to
    repository. Adds clang-format file.

---
+++ MainWindow.cpp
@@ -0,0 +1,10 @@
+#include "MainWindow.h"
+#include "./ui_MainWindow.h"
+
+MainWindow::MainWindow (QWidget *parent)
+    : QMainWindow (parent), ui (new Ui::MainWindow)
+{
+    ui->setupUi (this);
+}
+
+MainWindow::~MainWindow () { delete ui; }
